c7cb159d7573eb8a27c76695302d98fab1b5f9fb
thepythonistm <thepythonistm@gmail.com>
2025-10-06 11:36:51 -0700

    changes in apiClient

--- frontend/src/Components/apiClient.jsx
+++ frontend/src/Components/apiClient.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: "https://awall-space.onrender.com",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
